1bf85679dfd137c68578597983993828956fa252
Christian <christiannathanielp@gmail.com>
2024-10-26 15:52:11 +0700

    Progress 2 - Update Main Activity

--- app/src/main/java/com/example/uts_pm5_secondchoise/MainActivity.kt
+++ app/src/main/java/com/example/uts_pm5_secondchoise/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.uts_pm5_secondchoise
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uts_pm5_secondchoise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    Make Binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
